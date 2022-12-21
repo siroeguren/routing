324078aee53d3ce4fa97f7221c0d270e0fa32a67
siroeguren <siro41@hotmail.com>
2022-12-21 10:21:12 +0100

    pedro chepu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {path : 'users', component : UsersComponent},
+  {path : 'services', component : ServicesComponent},
+  {path : '', component : HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
